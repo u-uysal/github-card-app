f1681f069775f2d4d715fcf498c00fa5a5121131
Ufuk UYSAL <ufuk.uysl5@gmail.com>
2020-05-10 01:05:18 +0200

    Updates

---
+++ precache-manifest.7b06bed5e0204fa850d8e2640e4db66e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c84bde4b5c51287814926b05a03f127",
+    "url": "/github-card-app/index.html"
+  },
+  {
+    "revision": "dcd60d7f9c6c9f6b000a",
+    "url": "/github-card-app/static/css/main.f7208614.chunk.css"
+  },
+  {
+    "revision": "8c4b3dbf8318add48ba9",
+    "url": "/github-card-app/static/js/2.0ca1a05c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-card-app/static/js/2.0ca1a05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd60d7f9c6c9f6b000a",
+    "url": "/github-card-app/static/js/main.2d6b4b8c.chunk.js"
+  },
+  {
+    "revision": "2d5b1565cab8b3ad3dce",
+    "url": "/github-card-app/static/js/runtime-main.fba15fae.js"
+  }
+]);
